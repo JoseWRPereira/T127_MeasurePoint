35e6347c2b180d54491ca1ad9bd5e2ba24a3e266
JoseWRPereira <josewrpereira@gmail.com>
2021-05-07 15:44:00 -0300

    Projeto de leitura de temperatura via DHT11 e DHT22 e comunicação com plataforma ThingSpeak. Primeira versão.

---
+++ include/ts_connect.h
@@ -0,0 +1,35 @@
+#ifndef THINGSPEAK_CONNECT_H
+#define THINGSPEAK_CONNECT_H
+
+
+// WiFi authentication
+#define WIFI_SSID       "t127"
+#define WIFI_PASSWORD   "12345678" 
+// #define WIFI_SSID       "IoT_Hub"
+// #define WIFI_PASSWORD   "SENAI127iot"
+// #define WIFI_SSID       "SENAI_CORP"
+// #define WIFI_PASSWORD   "ac1ce2ss7"
+
+
+// ThingSpeak (TS) information
+#define TS_ADDRS            "api.thingspeak.com"
+#define TS_CHANNEL_ID       1381369
+#define TS_READAPIKEY       "CBWHCZ6TGSGJ6W7J"
+#define TS_WRITEAPIKEY      "6WFOK02JI7PJRJRA"
+#define TS_DATA_FIELD_TEMP  1
+#define TS_DATA_FIELD_UMID  2
+
+
+float readTSData( long TSChannel,unsigned int TSField );
+    
+    // Use this function if you want to write a single field.
+int writeTSData( long TSChannel, unsigned int TSField, float data );
+    
+    // Use this function if you want to write multiple fields simultaneously.
+int write2TSData( long TSChannel, unsigned int TSField1, float field1Data, unsigned int TSField2, long field2Data, unsigned int TSField3, long field3Data );
+
+void TS_connect_init( void );
+void TS_data_write( unsigned int TSField, float data );
+int TS_dht_write( unsigned int TSField1, float field1Data, unsigned int TSField2, float field2Data );
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <ESP8266WiFi.h>
+#include <ThingSpeak.h>
+#include "ts_connect.h"
+#include "dht.h"
+#include "filtro_media_movel.h"
+
+#define LED_PIN 16
+
+const unsigned long postingInterval = 1L * 1000L;
+unsigned long lastConnectionTime = 0;
+long lastUpdateTime = 0; 
+
+
+void setup() 
+{
+  Serial.begin(115200); 
+  delay(3000);
+
+  dht_init();
+
+  pinMode(LED_PIN, OUTPUT); 
+  digitalWrite(LED_PIN, 0); 
+
+  TS_connect_init();
+}
+
+void loop() 
+{
+  if( millis() - lastUpdateTime >=  postingInterval )
+  {    
+    float temp, umid;
+    lastUpdateTime = millis();
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN) );
+
+    temp = dht_temperatura();
+    Serial.print(F("Temperatura: "));
+    Serial.print( String(temp) );
+    Serial.println(F("°C"));
+
+    umid = dht_umidade();
+    Serial.print(F("Umidade: "));
+    Serial.print( String(umid));
+    Serial.println(F("%"));
+
+    if( !dht_error() )
+    {
+      int t_indice;
+      t_indice = fmm_add( temp );
+      if( !t_indice )
+      {
+        TS_dht_write( TS_DATA_FIELD_TEMP, temp,  TS_DATA_FIELD_UMID, umid );
+        Serial.println(F("TS"));
+      }
+    }
+  }
+}
